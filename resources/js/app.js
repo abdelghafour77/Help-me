add27ff54fe46b514b9db4b347075241c8b24565
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2023-04-08 03:43:05 +0000

    add components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,10 @@
 import './bootstrap';
+import 'flowbite';
+import './style.css';
+import 'flowbite/dist/flowbite.js';
+import './sidebar';
+import './charts';
+import './dark-mode';
 
 import Alpine from 'alpinejs';
 
