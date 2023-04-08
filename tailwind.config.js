2eb4ab6f626205298f9b4af0bfc33c8df88c71fd
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2023-04-08 18:16:55 +0000

    update config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,9 @@ module.exports = {
             fontFamily: {
                 'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
                 'body': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
-                'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace']
+                'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
+                'dancing': ['Dancing Script', 'cursive'],
+
             },
             transitionProperty: {
                 'width': 'width'
@@ -49,13 +51,7 @@ module.exports = {
     },
 
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
+
 
     plugins: [
         require('@tailwindcss/forms'),
