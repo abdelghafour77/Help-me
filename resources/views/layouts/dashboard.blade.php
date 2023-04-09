942990e47f586e916691d4ebf1a0e35e31d5df6e
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2023-04-09 19:59:25 +0000

    fix sidebar

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -32,6 +32,8 @@
 
         <script async defer src="https://buttons.github.io/buttons.js"></script>
         <script src="{{ asset('assets/app.bundle.js') }}"></script>
+        <script src="{{ asset('assets/js/sidebar.js') }}"></script>
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/datepicker.min.js"></script>
 </body>
 
