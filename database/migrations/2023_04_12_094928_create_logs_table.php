17a1819d9956aa48b02fbcd659fc6c77473ef061
Abdelghafour AOUAD <abdelghafour.aouad0@gmail.com>
2023-04-13 10:24:41 +0000

    fix issues in user

--- database/migrations/2023_04_12_094928_create_logs_table.php
+++ database/migrations/2023_04_12_094928_create_logs_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('message');
             $table->string('ip');
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->string('user_role');
             $table->string('url')->nullable();
             $table->timestamps();
